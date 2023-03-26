14e3fcd4f6df61268d084b3223e01706fd99e050
Elijah Greene <egreene017@gmail.com>
2023-03-26 18:36:06 -0400

    ready

--- src/directory/setup.Proxy.js
+++ src/directory/setup.Proxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
     app.use(
         createProxyMiddleware(['/api', , '/otherApi'], {
-            target: 'https://todo-backend122.herokuapp.com/',
+            target: 'backendhere',
         })
     );
 };
